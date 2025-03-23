6dc226f2e566acc25b6f8aab851b04e2d8282eae
Hermes <hermesbarbosa9@gmail.com>
2025-03-23 11:41:00 -0300

    Continuei o aprendizado dos tutoriais de langgraph

--- langgraph/chat.py
+++ langgraph/chat.py
@@ -1,7 +1,16 @@
 from main import graph
+from langgraph.types import Command
 
+human_response = (
+    "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent."
+    " It's much more reliable and extensible than simple autonomous agents."
+)
+
+human_command = Command(resume={"data": human_response})
+
+config = {"configurable": {"thread_id": "1"}}
 def stream_graph_update(user_input: str):
-    response = graph.invoke({'messages': user_input})
+    response = graph.invoke({'messages': user_input}, config, human_command)
     print("Assistent: ", response['messages'][-1].content)
 
 while True:
--- langgraph/main.py
+++ langgraph/main.py
@@ -1,11 +1,26 @@
 from typing import Annotated
-
 from typing_extensions import TypedDict
-
+from langchain_core.tools import tool
 from langgraph.graph import StateGraph, END, START
 from langgraph.graph.message import add_messages
-
 from langchain_google_vertexai import ChatVertexAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt.tool_node import ToolNode, tools_condition
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import Command, interrupt
+
+# Carrega as variáveis de ambiente
+load_dotenv()
+
+@tool
+def human_assistance(query: str) -> str:
+    """Requerer assistência humana"""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
+tool_tavily = TavilySearchResults(max_results=2)
+tools = [tool_tavily, human_assistance]
 
 class State(TypedDict):
     # As menssagens são do tipo "list"
@@ -18,13 +33,21 @@ graph_builder = StateGraph(State)
 llm = ChatVertexAI(model_name="gemini-1.5-flash")
 
 def chatbot(state: State):
-    return {'messages': [llm.invoke(state['messages'])]}
+    llm_with_tools = llm.bind_tools(tools)
+    return {'messages': [llm_with_tools.invoke(state['messages'])]}
 
+tools_node = ToolNode(tools)
+
+# Definindo os nós
 # O primeiro argumento é o nome exclusivo do nó
 # O segundo argumento é a função ou objeto que será usado sempre
+graph_builder.add_node('tools', tools_node)
 graph_builder.add_node('chatbot', chatbot)
+
+# Instruindo as pontes entre os nós
 graph_builder.add_edge(START, 'chatbot')
-graph_builder.add_edge('chatbot', END)
+graph_builder.add_conditional_edges('chatbot', tools_condition, {"tools": "tools", END: END})
+graph_builder.add_edge('tools', 'chatbot')
 
-graph = graph_builder.compile()
-
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
